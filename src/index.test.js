3cc64a50b697b766410b6f0a9055cfd9df3ac647
Gunnar Radzom <rad@Gunnars-MBP-Work.fritz.box>
2017-06-10 02:38:58 +0200

    refactor(tests): extracted check for uint32 into own function

--- src/index.test.js
+++ src/index.test.js
@@ -3,6 +3,15 @@ require('babel-polyfill');
 const { generateNonce } = require('../dist/index');
 const t = require('tap');
 
+function isUInt32(value) {
+  return (
+    typeof value === 'number' &&
+    value === Math.ceil(value) &&
+    value >= 0 &&
+    value < 2 ** 32
+  );
+}
+
 t.test('generateNonce', (t1) => {
   // arrange
   const sampleSize = 10;
@@ -12,15 +21,7 @@ t.test('generateNonce', (t1) => {
 
   // assert
   t1.ok(
-    nonces.reduce(
-      (aggregate, nonce) =>
-        aggregate &&
-        typeof nonce === 'number' &&
-        nonce === Math.ceil(nonce) &&
-        nonce >= 0 &&
-        nonce < 2 ** 32,
-      true,
-    ),
+    nonces.reduce((prev, nonce) => prev && isUInt32(nonce), true),
     'returns always an unsinged int32',
   );
   t1.ok(
